f4e2452091faed5e39049a56e9912897c175a985
Sebastian Kasanzew <sebkasanzew@gmail.com>
2017-09-26 01:05:31 +0200

    Minor comment updates

--- main.js
+++ main.js
@@ -11,8 +11,17 @@ if (isDev) {
 	})
 }
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the javascript object is garbage collected.
+let mainWindow
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+	if (process.platform != 'darwin')
+		app.quit()
+})
+
 app.on('ready', () => {
-	let mainWindow = null
 	let loading = new BrowserWindow({
 		width: 300,
 		height: 200,
@@ -55,6 +64,9 @@ app.on('ready', () => {
 	loading.show()
 
 	mainWindow.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
 		mainWindow = null
 	})
 })
